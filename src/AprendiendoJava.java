7df911173c6bb0d34eba3d2c5614771b44d87359
juanrealpe972 <juanrealpe972@gmail.com>
2024-09-17 23:56:29 -0500

    first commit

---
+++ src/AprendiendoJava.java
@@ -0,0 +1,10 @@
+public class AprendiendoJava {
+    public static void main(String[] args) throws Exception {
+        /*
+         * Comentarios multilíenas
+         */
+
+        //  Comentarios unitarios
+        System.out.println("Hello, World for Juanca!");
+    }
+}
