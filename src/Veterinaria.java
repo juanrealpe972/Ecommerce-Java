40fcf20d651527d10c42efc3ff1d629a629e3fbb
juanrealpe972 <juanrealpe972@gmail.com>
2024-09-22 02:09:16 -0500

    Trasfiriendo los archivos y ejemplos agregados aquí, al repositorio de practicando-java, ya que en este, se va a implementar la lógica necesaria para realizar el backend  o API REST  con CRUD a una base de datos Relacional en Postgresql.

--- src/Veterinaria.java
+++
@@ -1,4 +0,0 @@
-public class Veterinaria {
-    // Atributos o propiedades
-    static String nombre = "La Veterinaria de Juan";
-}
--- src/interfaces/Alimentable.java
+++
@@ -1,7 +0,0 @@
-package interfaces;
-
-public interface Alimentable {
-
-    // Metodo
-    void alimentarse();
-}
